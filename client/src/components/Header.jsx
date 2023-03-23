fe19e2fb9eaf3138a03a6f017e7383a68169aab7
talha saleem <Talha.saleem@devsinc.com>
2023-03-23 21:29:52 +0500

    logo added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
-
+import logo from "../assets/caption.png";
 const Header = () => {
   return (
     <Navbar color="light" light expand="md">
+
       <NavbarBrand style={{ fontWeight: "bold", textAlign: "center", width: "100%" }}>
+      <img src={logo} alt="Logo" style={{ height: "30px", marginRight: "10px" }} />
         AI Caption Generator
       </NavbarBrand>
     </Navbar>
